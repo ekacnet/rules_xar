5e49eaf9a8aa6010f955598f7e1aa263b105e416
Matthieu Patou <mat@matws.net>
2023-12-22 19:30:53 -0800

    Reorganize things a bit add a simple example too

--- examples/subdir/test.py
+++ examples/subdir/test.py
@@ -1,9 +1,7 @@
-from libfoo import foobar, foofoo
-from otherdir import bar
-import time
+from examples.subdir.libfoo import foobar, foofoo
+from examples.otherdir import bar
 
 print("Called")
-time.sleep(120)
 print(foofoo.func2())
 print(foobar.func1())
 print(bar.func3())
